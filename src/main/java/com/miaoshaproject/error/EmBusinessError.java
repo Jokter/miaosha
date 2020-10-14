dc9735eb02380596506ead42120f11f3e9222208
xiaoyao <1183663568@qq.com>
2020-10-14 09:44:35 +0800

    order提交

--- src/main/java/com/miaoshaproject/error/EmBusinessError.java
+++ src/main/java/com/miaoshaproject/error/EmBusinessError.java
@@ -8,7 +8,10 @@ public enum EmBusinessError implements CommonError {
 
 
     USER_NOT_EXIST(20001,"用户不存在"),
-    USER_LOGIN_FAIL(20002,"用户手机号或密码不正确")
+    USER_LOGIN_FAIL(20002,"用户手机号或密码不正确"),
+    USER_NOT_LOGIN(20003,"用户未登录"),
+
+    STOCK_NOT_ENOUGH(30001,"库存不足"),
     ;
 
     private EmBusinessError(int errCode,String errMsg){
