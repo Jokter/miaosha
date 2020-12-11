caccd0f38ac830a7a5c9c64758b70bcfafa1d73f
xiaoyao <1183663568@qq.com>
2020-12-11 09:27:33 +0800

    add

--- src/main/java/com/miaoshaproject/config/RedisConfig.java
+++ src/main/java/com/miaoshaproject/config/RedisConfig.java
@@ -1,8 +1,14 @@
 package com.miaoshaproject.config;
 
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Component;
+
 /**
  * @author wangyu
  * @created 2020/12/10 8:54 下午
  */
+@Component
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisConfig {
+
 }
--- src/main/resources/html/gethost.js
+++ src/main/resources/html/gethost.js
@@ -1,1 +1,2 @@
-var g_host="miaoshaserver";
+// var g_host="miaoshaserver";
+var g_host="localhost:8090";
